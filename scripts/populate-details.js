bec24b1959c604d544bfa5b067c88e5f40f4508b
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2024-12-08 01:13:22 +0100

    feat: migrate populate-details script to the new data source

--- scripts/populate-details.js
+++ scripts/populate-details.js
@@ -1,45 +1,79 @@
 const { writeFileSync } = require('fs')
 const path = require('path')
 
-const standards = require('../data/standards.json')
+const checks = require('../data/checks.json')
 
-const addContent = (content) => {
-  if (typeof content === 'string') {
-    return content
+const addImplementationDetails = (check) => {
+  if (!check.implementation_type) {
+    return ''
+  }
+  let content = `- Implementation Details: It is ${check.implementation_type}`
+  if (check.implementation_details_reference) {
+    content += ` ([details](${check.implementation_details_reference})).`
+  }
+  return content
+}
+
+const addContent = (title, description, url) => {
+  if (!description && !url) {
+    return ''
   }
 
-  return `[${content.description}](${content.url})`
+  if (url) {
+    return `- ${title}: [${description}](${url})`
+  }
+
+  return `- ${title}: ${description}`
 }
+
+const renderDetails = (check) => {
+  const implementationDetails = addImplementationDetails(check)
+  const mitreDetails = addContent('Mitre', check.mitre_description, check.mitre_url)
+  const sourcesDetails = addContent('Sources', check.sources_description, check.sources_url)
+  const howToDetails = addContent('How To', check.how_to_description, check.how_to_url)
+  let content = '## Details\n'
+  content += `- Implementation Status: ${check.implementation_status}\n`
+  if (implementationDetails) {
+    content += `${implementationDetails}\n`
+  }
+  content += `- C-SCRM: ${check.is_c_scrm}\n`
+  content += `- Priority Group: ${check.priority_group}\n`
+  if (mitreDetails) {
+    content += `${mitreDetails}\n`
+  }
+  if (sourcesDetails) {
+    content += `${sourcesDetails}\n`
+  }
+  if (howToDetails) {
+    content += `${howToDetails}\n`
+  }
+  content += `- Created at ${check.created_at}\n`
+  content += `- Updated at ${check.updated_at}\n`
+  return content
+}
+
 // Prepare the markdown files
-standards.forEach((item, index) => {
+checks.forEach((check, index) => {
   const fileContent = `---
 sidebar_position: ${index + 1}
-id: ${item.slug}
-title: ${item.title}
-slug: /details/${item.slug}
+id: ${check.id}
+title: ${check.title}
+slug: /details/${check.code_name}
 ---
 
-# ${item.title}
+# ${check.title}
 
 ## Use Case
-
-- Incubating: ${item.incubating}
-- Active: ${item.active}
-- Retiring: ${item.retiring}
+- Incubating: ${check.level_incubating_status}
+- Active: ${check.level_active_status}
+- Retiring: ${check.level_retiring_status}
 
 ## Description
 
-${item.description}
+${check.description}
 
-## Details
-
-- C-SCRM: ${item['c-scrm']}
-- Priority Group: ${item['priority group']}
-- Mitre: ${addContent(item.mitre)}
-- Sources: ${addContent(item.sources)}
-- How To: ${addContent(item['how to'])}
-
+${renderDetails(check)}
 `
-  const detination = path.join(process.cwd(), `docs/details/${item.slug}.mdx`)
+  const detination = path.join(process.cwd(), `docs/details/${check.code_name}.mdx`)
   writeFileSync(detination, fileContent)
 })
